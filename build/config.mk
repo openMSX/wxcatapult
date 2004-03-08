c7a9a445dffcfe6df404a54dbf828543b4b5af5d
Maarten ter Huurne <maarten@treewalker.org>
2004-03-08 20:42:57 +0000

    Oops... forgot to add this two days ago.
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@8671 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/config.mk
@@ -0,0 +1,9 @@
+# $Id$
+#
+# This file contains user-adjustable settings for the build and installation
+# process.
+
+# Top of the installation directory.
+# Catapult installs as a self-contained directory containing all elements,
+# such as the binary, the resources, the documentation etc.
+INSTALL_BASE:=/opt/openMSX-Catapult
