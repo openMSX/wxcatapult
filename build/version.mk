edb7eebb7b52a3f9e2ad6f910504518915332b63
Maarten ter Huurne <maarten@treewalker.org>
2005-06-18 22:11:06 +0000

    Set version for 0.5.2 release.
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@9055 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -1,4 +1,4 @@
-# $Id: version.mk,v 1.12 2005/03/05 22:00:55 mthuurne Exp $
+# $Id: version.mk,v 1.13 2005/03/05 23:22:59 mthuurne Exp $
 #
 # Version info.
 
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx-catapult
 
 # Version number.
-PACKAGE_VERSION:=0.5.1-R1
+PACKAGE_VERSION:=0.5.2-R1
 
 # Is this a release version ("true") or development version ("false").
-RELEASE_FLAG:=false
+RELEASE_FLAG:=true
