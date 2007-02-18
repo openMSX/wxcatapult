b351958372a8cd539758c1a995cb779ed3ff7963
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2007-02-18 11:11:53 +0000

    Updates for release 0.6.2-R1, release date still needs to be filled in
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@9183 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/version.mk
+++ build/version.mk
@@ -1,4 +1,4 @@
-# $Id: version.mk,v 1.20 2006/01/28 00:09:52 manuelbi Exp $
+# $Id: version.mk,v 1.21 2006/07/30 17:21:45 manuelbi Exp $
 #
 # Version info.
 
@@ -6,7 +6,7 @@
 PACKAGE_NAME:=openmsx-catapult
 
 # Version number.
-PACKAGE_VERSION:=0.6.1-R1
+PACKAGE_VERSION:=0.6.2-R1
 
 # Is this a release version ("true") or development version ("false").
 RELEASE_FLAG:=true
