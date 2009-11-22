c1bfc24ea026170d7af03435fdaa1bcb74251345
Maarten ter Huurne <maarten@treewalker.org>
2009-11-22 16:45:20 +0000

    Cosmetic fixes.
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@10810 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-mingw32.mk
+++ build/platform-mingw32.mk
@@ -1,6 +1,6 @@
 # $Id$
 #
-# Configuration for MinGW on x86 machines.
+# Configuration for MinGW.
 
 # Does platform support symlinks?
 USE_SYMLINK:=false
@@ -8,7 +8,7 @@ USE_SYMLINK:=false
 # File name extension of executables.
 EXEEXT:=.exe
 
-# The next lines are needed to determen the name of the XRC library from wx-config
+# The next line is needed to determine the name of the XRC library from wx-config.
 WX2XRC_DYNAMIC:=s/-lwx\\([^0-9]*\\)\\([0-9]\\)\\([0-9]\\)./ -lwx_\\1_xrc-\\2\\.\\3 & /
 WX2XRC_STATIC:=s/\\(\\/[^ ]*\\/\\)libwx\\([^0-9]*\\)\\([0-9]\\)\\([0-9]\\).\\.a/ \\1libwx_\\2_xrc-\\3\\.\\4\\.a & /
 
@@ -25,10 +25,5 @@ LINK_FLAGS:=-L/mingw/lib -L/mingw/lib/w32api \
 	`if test -d /usr/local/lib; then echo '-L/usr/local/lib'; fi` \
 	$(LINK_FLAGS)
 
-#platform specific sourcefiles
-SOURCES+= openMSXWindowsController \
-	PipeConnectThread
-	
-
-
-
+# Platform specific source files.
+SOURCES+=openMSXWindowsController PipeConnectThread
