a9bc4f7e72bd698f36433248eccb20a92af4c566
Maarten ter Huurne <maarten@treewalker.org>
2009-11-22 16:40:17 +0000

    Use FreeBSD configuration for NetBSD too.
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@10807 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/platform-netbsd.mk
+++ build/platform-netbsd.mk
@@ -1,18 +1,6 @@
 # $Id$
 #
-# Configuration for Linux on x86 machines.
+# Configuration for NetBSD.
 
-# Does platform support symlinks?
-USE_SYMLINK:=true
-
-# File name extension of executables.
-EXEEXT:=
-
-# The next line is needed to determen the name of the XRC library from wx-config
-WX2XRC_DYNAMIC:=s/-lwx_\\([^-]*\\)-\\([^ ]*\\)/& -lwx_\\1_xrc-\\2/
-
-#platform specific sourcefiles
-SOURCES+= openMSXLinuxController
-
-CXXFLAGS+=-D_REENTRANT -D_THREAD_SAFE
-LINK_FLAGS+=-pthread 
+# Assume NetBSD is a lot like FreeBSD.
+include build/platform-freebsd.mk
