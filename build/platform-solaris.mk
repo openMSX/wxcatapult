4747fc4c376b40dd99a75e6fecf5a9a031def7e5
Maarten ter Huurne <maarten@treewalker.org>
2009-11-22 16:48:08 +0000

    Based on Linux configuration; untested.
    git-svn-id: file:///openmsx-svn/code/wxcatapult/trunk@10813 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/platform-solaris.mk
@@ -0,0 +1,15 @@
+# $Id$
+#
+# Configuration for Solaris machines.
+
+# Does platform support symlinks?
+USE_SYMLINK:=true
+
+# File name extension of executables.
+EXEEXT:=
+
+# The next line is needed to determine the name of the XRC library from wx-config.
+WX2XRC_DYNAMIC:=s/-lwx_\\([^-]*\\)-\\([^ ]*\\)/& -lwx_\\1_xrc-\\2/
+
+# Platform specific source files.
+SOURCES+=openMSXLinuxController
