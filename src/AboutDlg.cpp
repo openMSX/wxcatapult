19762fda667385930ad3bbf9c5781ebf5b63d9d9
wouter <vermaelen.wouter@gmail.com>
2013-07-28 22:35:45 +0200

    Removed windows specific code from AboutDlg
    Vampier verified for me that it still works fine without this code. Possibly
    this was a workaround for a (windows-specific) bug in an older version of
    wxWindows??

--- src/AboutDlg.cpp
+++ src/AboutDlg.cpp
@@ -1,8 +1,6 @@
 #include "AboutDlg.h"
 #include <wx/wxprec.h>
 #include <wx/xrc/xmlres.h>
-#include <wx/image.h>
-#include <wx/statbmp.h>
 
 IMPLEMENT_CLASS(AboutDlg, wxDialog)
 BEGIN_EVENT_TABLE(AboutDlg, wxDialog)
@@ -12,16 +10,6 @@ END_EVENT_TABLE()
 AboutDlg::AboutDlg(wxWindow* parent)
 {
 	wxXmlResource::Get()->LoadDialog(this, parent, wxT("AboutDialog"));
-#ifdef __WXMSW__  // why is this windows specific??
-//	auto* aboutButton = (wxBitmapButton*)FindWindowByName(wxT("AboutButton"));
-	auto* aboutBitmap = (wxStaticBitmap*)FindWindowByName(wxT("AboutBitmap"));
-//	wxBitmap bmp = aboutButton->GetBitmapLabel();
-	wxBitmap bmp = aboutBitmap->GetBitmap();
-	wxImage img = bmp.ConvertToImage();
-	img.ConvertAlphaToMask(100);
-	wxBitmap bmp2(img);
-	aboutBitmap->SetBitmap(bmp2);
-#endif
 }
 
 void AboutDlg::OnClose(wxCommandEvent& event)
