8f8c5b6df3f88c8dbc08cc649d7cdf0d52a75f9b
wouter <vermaelen.wouter@gmail.com>
2013-08-02 14:05:01 +0200

    Cleanups in CatapultPage
    These are the most important:
    - Replace statically-sized m_settingTable with a std::vector (that grows
      automatically).
    - Introduce a isTclTrue() helper function to factor out some duplicated code
      from various places (and simplify some boolean logic related to this).
    - Pass wxString parameters as 'const wxString&', this avoids making copies
      all the time.

--- src/CatapultPage.cpp
+++ src/CatapultPage.cpp
@@ -2,7 +2,6 @@
 #include "AudioControlPage.h"
 #include "VideoControlPage.h"
 #include "ConfigurationData.h"
-//#include "wx/version.h"
 #include <wx/msgdlg.h>
 #include <wx/combobox.h>
 #include <wx/frame.h>
@@ -12,20 +11,63 @@
 #include <wx/wxprec.h>
 #include <wx/xrc/xmlres.h>
 
-#define S_CONVERT 1
-#define S_EVENT 2
-#define S_INVERT 4
-#define SETTINGTABLE_MAXSIZE 37
+static const int S_CONVERT = 1;
+static const int S_EVENT   = 2;
+static const int S_INVERT  = 4;
 
 CatapultPage::CatapultPage(wxWindow* parent)
 	: m_parent(parent)
 {
-	InitSettingsTable();
+	AddSetting(wxT("renderer"), wxT("RendererSelector"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("scale_algorithm"), wxT("ScalerAlgoSelector"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("scale_factor"), wxT("ScalerFactorSelector"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("accuracy"), wxT("AccuracySelector"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("deinterlace"), wxT("DeInterlaceButton"), &CatapultPage::UpdateToggle, S_CONVERT);
+	AddSetting(wxT("limitsprites"), wxT("LimitSpriteButton"), &CatapultPage::UpdateToggle, S_CONVERT);
+	AddSetting(wxT("blur"), wxT("BlurIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("glow"), wxT("GlowIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("gamma"), wxT("GammaIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("scanline"), wxT("ScanlineIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("speed"), wxT("SpeedIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("minframeskip"), wxT("MinFrameSkipIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("maxframeskip"), wxT("MaxFrameSkipIndicator"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("throttle"), wxT("MaxSpeedButton"), &CatapultPage::UpdateToggle, S_INVERT | S_EVENT);
+	AddSetting(wxT("power"), wxT("PowerButton"), &CatapultPage::UpdateToggle);
+	AddSetting(wxT("pause"), wxT("PauseButton"), &CatapultPage::UpdateToggle);
+	AddSetting(wxT("frontswitch"), wxT("FirmwareButton"), &CatapultPage::UpdateToggle);
+	AddSetting(wxT("firmwareswitch"), wxT("FirmwareButton"), &CatapultPage::UpdateToggle);
+	AddSetting(wxT("mute"), wxT("MuteButton"), &CatapultPage::UpdateToggle);
+	AddSetting(wxT("midi-in-readfilename"), wxT("MidiInFileInput"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("midi-out-logfilename"), wxT("MidiOutFileInput"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("audio-inputfilename"), wxT("SampleFileInput"), &CatapultPage::UpdateIndicator);
+	AddSetting(wxT("*_volume"), wxT("volume"), &CatapultPage::UpdateAudio);
+	AddSetting(wxT("*_balance"), wxT("balance"), &CatapultPage::UpdateAudio);
+	AddSetting(wxT("msx-midi-in"), wxT("MidiInSelector"), &CatapultPage::UpdateMidiPlug);
+	AddSetting(wxT("msx-midi-out"), wxT("MidiOutSelector"), &CatapultPage::UpdateMidiPlug);
+	AddSetting(wxT("pcminput"), wxT("SampleInSelector"), &CatapultPage::UpdatePluggable);
+	AddSetting(wxT("joyporta"), wxT("Joyport1Selector"), &CatapultPage::UpdatePluggable);
+	AddSetting(wxT("joyportb"), wxT("Joyport2Selector"), &CatapultPage::UpdatePluggable);
+	AddSetting(wxT("printerport"), wxT("PrinterportSelector"), &CatapultPage::UpdatePluggable);
+	AddSetting(wxT("renshaturbo"), wxT("RenshaTurboSlider"), &CatapultPage::UpdateSlider);
+	AddSetting(wxT("diska"), wxT("DiskAContents"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("diskb"), wxT("DiskBContents"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("cassetteplayer"), wxT("CassetteContents"), &CatapultPage::UpdateCombo);
+	AddSetting(wxT("fullscreen"), wxT("FullScreenButton"), &CatapultPage::UpdateToggle, S_CONVERT);
+	AddSetting(wxT("save_settings_on_exit"), wxT("Save Settings On Exit"), &CatapultPage::UpdateMenu);
+	AddSetting(wxT("printerlogfilename"), wxT("PrinterLogFile"),&CatapultPage::UpdateIndicator, S_CONVERT);
 }
 
-CatapultPage::~CatapultPage()
+void CatapultPage::AddSetting(
+	const wxString& setting, const wxString& control,
+	void (CatapultPage::*pfunction)(const wxString&, const wxString&, const wxString&, int),
+	int flags)
 {
-	delete[] m_settingTable;
+	SettingElement elem;
+	elem.setting   = setting;
+	elem.control   = control;
+	elem.pfunction = pfunction;
+	elem.flags     = flags;
+	m_settingTable.push_back(elem);
 }
 
 wxString CatapultPage::ConvertPath(wxString path, bool convertSlash, bool backwards)
@@ -55,113 +97,34 @@ void CatapultPage::OnClickCombo(wxCommandEvent& event)
 	}
 }
 
-void CatapultPage::InitSettingsTable ()
+void CatapultPage::UpdateSetting(const wxString& name_, const wxString& data)
 {
-	m_settingTableSize = 0;
-	m_settingTable = new SettingTableElementType[SETTINGTABLE_MAXSIZE];
-	AddSetting(wxT("renderer"), wxT("RendererSelector"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("scale_algorithm"), wxT("ScalerAlgoSelector"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("scale_factor"), wxT("ScalerFactorSelector"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("accuracy"), wxT("AccuracySelector"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("deinterlace"), wxT("DeInterlaceButton"), &CatapultPage::UpdateToggleSetting, S_CONVERT);
-	AddSetting(wxT("limitsprites"), wxT("LimitSpriteButton"), &CatapultPage::UpdateToggleSetting, S_CONVERT);
-	AddSetting(wxT("blur"), wxT("BlurIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("glow"), wxT("GlowIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("gamma"), wxT("GammaIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("scanline"), wxT("ScanlineIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("speed"), wxT("SpeedIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("minframeskip"), wxT("MinFrameSkipIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("maxframeskip"), wxT("MaxFrameSkipIndicator"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("throttle"), wxT("MaxSpeedButton"), &CatapultPage::UpdateToggleSetting, S_INVERT | S_EVENT);
-	AddSetting(wxT("power"), wxT("PowerButton"), &CatapultPage::UpdateToggleSetting, 0);
-	AddSetting(wxT("pause"), wxT("PauseButton"), &CatapultPage::UpdateToggleSetting, 0);
-	AddSetting(wxT("frontswitch"), wxT("FirmwareButton"), &CatapultPage::UpdateToggleSetting, 0);
-	AddSetting(wxT("firmwareswitch"), wxT("FirmwareButton"), &CatapultPage::UpdateToggleSetting, 0);
-	AddSetting(wxT("mute"), wxT("MuteButton"), &CatapultPage::UpdateToggleSetting, 0);
-	AddSetting(wxT("midi-in-readfilename"), wxT("MidiInFileInput"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("midi-out-logfilename"), wxT("MidiOutFileInput"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("audio-inputfilename"), wxT("SampleFileInput"), &CatapultPage::UpdateIndicatorSetting, 0);
-	AddSetting(wxT("*_volume"), wxT("volume"), &CatapultPage::UpdateAudioSetting, 0);
-	AddSetting(wxT("*_balance"), wxT("balance"), &CatapultPage::UpdateAudioSetting, 0);
-	AddSetting(wxT("msx-midi-in"), wxT("MidiInSelector"), &CatapultPage::UpdateMidiPlug, 0);
-	AddSetting(wxT("msx-midi-out"), wxT("MidiOutSelector"), &CatapultPage::UpdateMidiPlug, 0);
-	AddSetting(wxT("pcminput"), wxT("SampleInSelector"), &CatapultPage::UpdatePluggable, 0);
-	AddSetting(wxT("joyporta"), wxT("Joyport1Selector"), &CatapultPage::UpdatePluggable, 0);
-	AddSetting(wxT("joyportb"), wxT("Joyport2Selector"), &CatapultPage::UpdatePluggable, 0);
-	AddSetting(wxT("printerport"), wxT("PrinterportSelector"), &CatapultPage::UpdatePluggable, 0);
-	AddSetting(wxT("renshaturbo"), wxT("RenshaTurboSlider"), &CatapultPage::UpdateSliderSetting, 0);
-	AddSetting(wxT("diska"), wxT("DiskAContents"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("diskb"), wxT("DiskBContents"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("cassetteplayer"), wxT("CassetteContents"), &CatapultPage::UpdateComboSetting, 0);
-	AddSetting(wxT("fullscreen"), wxT("FullScreenButton"), &CatapultPage::UpdateToggleSetting, S_CONVERT);
-	AddSetting(wxT("save_settings_on_exit"), wxT("Save Settings On Exit"), &CatapultPage::UpdateMenuSetting, 0);
-	AddSetting(wxT("printerlogfilename"), wxT("PrinterLogFile"),&CatapultPage::UpdateIndicatorSetting, S_CONVERT);
-}
+	wxString name = name_; // TODO HACK: need a proper Tcl parser
+	name.Replace(wxT("\\"), wxT(""), true);
 
-void CatapultPage::AddSetting(
-	wxString setting, wxString controlname,
-	void (CatapultPage::*pfunction)(wxString, wxString, wxString, int),
-	int flags)
-{
-	if (m_settingTableSize >= SETTINGTABLE_MAXSIZE) {
-		wxMessageBox(wxT("Not enough space to store the setting table!\nPlease contact the authors."),
-		             wxT("Internal Catapult Error"));
-		return;
-	}
-	m_settingTable[m_settingTableSize].setting = setting;
-	m_settingTable[m_settingTableSize].controlname = controlname;
-	m_settingTable[m_settingTableSize].pfunction = pfunction;
-	m_settingTable[m_settingTableSize].flags = flags;
-	m_settingTableSize++;
-}
-
-static wxString removeBackslash(const wxString& s)
-{
-	wxString result;
-	int len = s.Length();
-	for (int i = 0; i < len; ++i) {
-		if (s[i] != '\\') {
-			result += s[i];
+	for (auto& elem : m_settingTable) {
+		if (name.Matches(elem.setting.c_str())) {
+			(*this.*(elem.pfunction))(
+				name, data, elem.control, elem.flags);
+			break;
 		}
 	}
-	return result;
-}
-void CatapultPage::UpdateSetting(wxString name, wxString data)
-{
-	name = removeBackslash(name); // !!! HACK !!!
-	int index = 0;
-	bool found = false;
-	while ((!found) && (index < SETTINGTABLE_MAXSIZE)) {
-		if (name.Matches(m_settingTable[index].setting.c_str())) {
-			found = true;
-			if (m_settingTable[index].pfunction != NULL) {
-				(*this.*(m_settingTable[index].pfunction))(
-					name, data, m_settingTable[index].controlname,
-					m_settingTable[index].flags);
-			}
-		}
-		++index;
-	}
 }
 
-void CatapultPage::UpdateToggleSetting(wxString setting, wxString data, wxString control, int flags)
+static bool isTclTrue(const wxString& str)
 {
-	bool value[] = {false, true};
-	int active = 1;
-	int sendvalue;
+	return (str == wxT("on"))   ||
+	       (str == wxT("true")) ||
+	       (str == wxT("1"))    ||
+	       (str == wxT("yes"));
+}
 
-	if (flags & S_INVERT) {
-		active = 0;
-	}
-
+void CatapultPage::UpdateToggle(const wxString& setting, const wxString& data, const wxString& control, int flags)
+{
 	if (auto* button = (wxToggleButton*)m_parent->FindWindowByName(control)) {
-		if ((data == wxT("on")) || (data == wxT("true")) || (data == wxT("1")) || (data == wxT("yes"))) {
-			sendvalue = active;
-		} else {
-			sendvalue = 1 - active;
-
-		}
-		button->SetValue(value[sendvalue]);
+		bool active = !(flags & S_INVERT);
+		bool sendvalue = isTclTrue(data) ? active : !active;
+		button->SetValue(sendvalue);
 
 		if (flags & S_EVENT) {
 			wxCommandEvent event(wxEVT_COMMAND_TOGGLEBUTTON_CLICKED, button->GetId());
@@ -171,18 +134,12 @@ void CatapultPage::UpdateToggleSetting(wxString setting, wxString data, wxString
 		}
 
 		if (flags & S_CONVERT) {
-			if ((data == wxT("false")) || (data == wxT("no")) || (data == wxT("0"))) {
-				data = wxT("off");
-			}
-			if ((data == wxT("true")) || (data == wxT("yes")) || (data == wxT("1"))) {
-				data = wxT("on");
-			}
-			button->SetLabel(data.Mid(0, 1).Upper() + data.Mid(1).Lower());
+			button->SetLabel(isTclTrue(data) ? wxT("On") : wxT("Off"));
 		}
 	}
 }
 
-void CatapultPage::UpdateComboSetting(wxString setting, wxString data, wxString control, int flags)
+void CatapultPage::UpdateCombo(const wxString& setting, const wxString& data, const wxString& control, int flags)
 {
 	wxString valuetext = data;
 	if (flags & S_CONVERT) {
@@ -197,7 +154,7 @@ void CatapultPage::UpdateComboSetting(wxString setting, wxString data, wxString 
 	}
 }
 
-void CatapultPage::UpdateIndicatorSetting(wxString setting, wxString data, wxString control, int flags)
+void CatapultPage::UpdateIndicator(const wxString& setting, const wxString& data, const wxString& control, int flags)
 {
 	if (auto* indicator = (wxTextCtrl*)m_parent->FindWindowByName(control)) {
 		wxString tempData = data;
@@ -210,7 +167,7 @@ void CatapultPage::UpdateIndicatorSetting(wxString setting, wxString data, wxStr
 	}
 }
 
-void CatapultPage::UpdateSliderSetting(wxString setting, wxString data, wxString control, int flags)
+void CatapultPage::UpdateSlider(const wxString& setting, const wxString& data, const wxString& control, int flags)
 {
 	if (auto* slider = (wxSlider*)m_parent->FindWindowByName(control)) {
 		long value;
@@ -221,20 +178,16 @@ void CatapultPage::UpdateSliderSetting(wxString setting, wxString data, wxString
 	}
 }
 
-void CatapultPage::UpdateMenuSetting(wxString setting, wxString data, wxString control, int flags)
+void CatapultPage::UpdateMenu(const wxString& setting, const wxString& data, const wxString& control, int flags)
 {
-	bool sendvalue = false;
 	auto* appwnd = (wxFrame*)GetParent()->GetGrandParent();
 	int menusetting = appwnd->GetMenuBar()->FindMenuItem(wxT("&Settings"), wxT("Save openMSX Settings On &Exit"));
 	if (menusetting != wxNOT_FOUND) {
-		if ((data == wxT("on")) || (data == wxT("true")) || (data == wxT("1")) || (data== wxT("yes"))) {
-			sendvalue = true;
-		}
-		appwnd->GetMenuBar()->Check(menusetting, sendvalue);
+		appwnd->GetMenuBar()->Check(menusetting, isTclTrue(data));
 	}
 }
 
-void CatapultPage::UpdateAudioSetting(wxString setting, wxString data, wxString selection, int flags)
+void CatapultPage::UpdateAudio(const wxString& setting, const wxString& data, const wxString& selection, int flags)
 {
 	auto* notebook = (wxNotebook*)m_parent;
 	AudioControlPage* audiopage = nullptr;
@@ -255,7 +208,7 @@ void CatapultPage::UpdateAudioSetting(wxString setting, wxString data, wxString 
 	}
 }
 
-void CatapultPage::UpdateMidiPlug(wxString connector, wxString data, wxString control, int flags)
+void CatapultPage::UpdateMidiPlug(const wxString& connector, const wxString& data, const wxString& control, int flags)
 {
 	auto* notebook = (wxNotebook*)m_parent;
 	AudioControlPage* audiopage = nullptr;
@@ -269,7 +222,7 @@ void CatapultPage::UpdateMidiPlug(wxString connector, wxString data, wxString co
 	audiopage->UpdateMidiPlug(control, data);
 }
 
-void CatapultPage::UpdatePluggable(wxString connector, wxString data, wxString control, int flags)
+void CatapultPage::UpdatePluggable(const wxString& connector, const wxString& data, const wxString& control, int flags)
 {
 	if (auto* box = (wxComboBox*)m_parent->FindWindowByName(control)) {
 		box->SetValue(data.IsEmpty() ? wxT("--empty--") : data);
--- src/CatapultPage.h
+++ src/CatapultPage.h
@@ -3,43 +3,43 @@
 
 #include <wx/panel.h>
 #include <wx/tglbtn.h>
+#include <vector>
 
 class CatapultPage : public wxPanel
 {
 public:
 	CatapultPage(wxWindow* parent = nullptr);
-	virtual ~CatapultPage();
 
-	void UpdateSetting(wxString name, wxString data);
-	void OnClickCombo (wxCommandEvent& event);
+	void UpdateSetting(const wxString& name, const wxString& data);
+	void OnClickCombo(wxCommandEvent& event);
+
 	virtual void HandleFocusChange(wxWindow* oldFocus, wxWindow* newFocus) = 0;
-	wxString ConvertPath(wxString path, bool convertSlash = false, bool backwards = false); // need to move somewhere else
+
+	static wxString ConvertPath(wxString path, bool convertSlash = false, bool backwards = false); // need to move somewhere else
 
 private:
-	void InitSettingsTable();
 	void AddSetting(
-		wxString settings, wxString controlname,
-		void (CatapultPage::*pfunction)(wxString, wxString, wxString, int),
-		int flags);
-	void UpdateToggleSetting   (wxString setting,   wxString data, wxString control,   int flags);
-	void UpdateComboSetting    (wxString setting,   wxString data, wxString control,   int flags);
-	void UpdateIndicatorSetting(wxString setting,   wxString data, wxString control,   int flags);
-	void UpdateSliderSetting   (wxString setting,   wxString data, wxString control,   int flags);
-	void UpdateAudioSetting    (wxString setting,   wxString data, wxString selection, int flags);
-	void UpdateMenuSetting     (wxString setting,   wxString data, wxString selection, int flags);
-	void UpdateMidiPlug        (wxString connector, wxString data, wxString control,   int flags);
-	void UpdatePluggable       (wxString connector, wxString data, wxString control,   int flags);
+		const wxString& settings, const wxString& control,
+		void (CatapultPage::*pfunction)(const wxString&, const wxString&, const wxString&, int),
+		int flags = 0);
+	void UpdateToggle   (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateCombo    (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateIndicator(const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateSlider   (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateAudio    (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateMenu     (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdateMidiPlug (const wxString& setting, const wxString& data, const wxString& control, int flags);
+	void UpdatePluggable(const wxString& setting, const wxString& data, const wxString& control, int flags);
 
-	struct SettingTableElementType {
+	struct SettingElement {
 		wxString setting;
-		wxString controlname;
-		void (CatapultPage::*pfunction)(wxString, wxString, wxString, int);
+		wxString control;
+		void (CatapultPage::*pfunction)(const wxString&, const wxString&, const wxString&, int);
 		int flags;
 	};
 
 	wxWindow* m_parent;
-	SettingTableElementType* m_settingTable;
-	int m_settingTableSize;
+	std::vector<SettingElement> m_settingTable;
 };
 
 #endif
