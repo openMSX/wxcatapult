7ab682f13fc6e78c37b7dd7ed36926612b86405d
Maarten ter Huurne <maarten@treewalker.org>
2014-01-04 12:00:03 +0100

    Enable more warnings

--- build/main.mk
+++ build/main.mk
@@ -262,7 +262,7 @@ DEPEND_FLAGS+=-MP
 # Compiler flags
 # ==============
 
-CXXFLAGS+=-pipe -Wall --no-strict-aliasing
+CXXFLAGS+=-pipe -Wall -Wextra -Wno-unused-parameter --no-strict-aliasing
 CXXFLAGS+=-Wno-literal-suffix -Wno-unused-local-typedefs
 CXXFLAGS+=-I$(CONFIG_PATH)
 CXXFLAGS+=$(XRC_CFLAGS) $(XML_CFLAGS)
