openMSX/wxcatapult

commit 4e56925b15c97b856652a0bae4d1ca80aa976300

Call SaveHistory() (only) from AddHistory()
SaveHistory() was always called (soon) after AddHistory(). And if it wasn't
that was a bug. So it's simpler to just make SaveHistory() part of
AddHistory().

src/SessionPage.cpp
@@ -361,7 +361,6 @@ void SessionPage::BrowseDisk(MediaInfo& m)
 			m_controller.WriteCommand(m.deviceName + wxT(" ") + utils::ConvertPath(m.contents));
 			if (m_controller.IsOpenMSXRunning()) {
 				AddHistory(m);
-				SaveHistory();
 				m.ips.Clear();
 				m.menu.SetLabel(Disk_Browse_Ips, wxT("Select IPS Patches (None selected)"));
 			}
@@ -392,7 +391,6 @@ void SessionPage::BrowseCart(MediaInfo& m)
 			m_controller.WriteCommand(m.deviceName + wxT(" ") + utils::ConvertPath(m.contents));
 			if (m_controller.IsOpenMSXRunning()) {
 				AddHistory(m);
-				SaveHistory();
 				m.ips.Clear();
 				m.type.Clear();
 				m.menu.SetLabel(Cart_Browse_Ips, wxT("Select IPS Patches (None selected)"));
@@ -416,7 +414,6 @@ void SessionPage::OnBrowseCassette(wxCommandEvent& event)
 			m_controller.WriteCommand(wxT("cassetteplayer ") + utils::ConvertPath(media[CAS]->contents));
 			if (m_controller.IsOpenMSXRunning()) {
 				AddHistory(*media[CAS]);
-				SaveHistory();
 			}
 		} else {
 			m_controller.WriteCommand(wxT("cassetteplayer eject"));
@@ -706,7 +703,6 @@ void SessionPage::HandleFocusChange(wxWindow* oldFocus, wxWindow* newFocus)
 	checkLooseFocus(oldFocus, *media[CARTA]);
 	checkLooseFocus(oldFocus, *media[CARTB]);
 	checkLooseFocus(oldFocus, *media[CAS]);
-	SaveHistory();
 }
 void SessionPage::checkLooseFocus(wxWindow* oldFocus, MediaInfo& m)
 {
@@ -833,7 +829,6 @@ void SessionPage::UpdateSessionData()
 	for (size_t i = 1; i < hardware.GetCount(); ++i) {
 		m_usedExtensions << hardware[i] << wxT("::");
 	}
-	SaveHistory();
 }
 
 void SessionPage::AddHistory(MediaInfo& m)
@@ -869,6 +864,8 @@ void SessionPage::AddHistory(MediaInfo& m)
 		m.control.Append(m.history[i]);
 	}
 	m.control.SetSelection(0);
+
+	SaveHistory();
 }
 
 void SessionPage::RestoreHistory()
@@ -1063,7 +1060,6 @@ void SessionPage::OnInsertEmptyDiskByMenu(wxCommandEvent& event)
 					m_controller.WriteCommand(target->deviceName + wxT(" eject"));
 				}
 				AddHistory(*target);
-				SaveHistory();
 			}
 		}
 	}

commit 7734301552aa80ffc37bb4d1d84f8539a2706127

Fix regression: save history for extension/machine list.
Whenever you change something to the extension/machine selection, save
the history, so that at next startup the selection is preserved. This
got broken in 4e56925b15c97b856652a0bae4d1ca80aa976300.

Apparently there was also a bug in getHardware which got triggered by
this change.

src/SessionPage.cpp
@@ -54,42 +54,44 @@ enum {
 
 IMPLEMENT_CLASS(SessionPage, wxPanel)
 BEGIN_EVENT_TABLE(SessionPage, wxPanel)
-	EVT_COMBOBOX(XRCID("DiskAContents"), SessionPage::OnClickDiskACombo)
-	EVT_COMBOBOX(XRCID("DiskBContents"), SessionPage::OnClickDiskBCombo)
-	EVT_COMBOBOX(XRCID("CartAContents"), SessionPage::OnClickCartACombo)
-	EVT_COMBOBOX(XRCID("CartBContents"), SessionPage::OnClickCartBCombo)
-	EVT_COMBOBOX(XRCID("CassetteContents"), SessionPage::OnClickCassetteCombo)
-	EVT_TOGGLEBUTTON(XRCID("PlayButton"), SessionPage::OnModePlay)
-	EVT_TOGGLEBUTTON(XRCID("RecordButton"), SessionPage::OnModeRecord)
-	EVT_BUTTON(XRCID("DiskA_Button"), SessionPage::OnClickMediaMenu)
-	EVT_BUTTON(XRCID("DiskB_Button"), SessionPage::OnClickMediaMenu)
-	EVT_BUTTON(XRCID("CartA_Button"), SessionPage::OnClickMediaMenu)
-	EVT_BUTTON(XRCID("CartB_Button"), SessionPage::OnClickMediaMenu)
-	EVT_BUTTON(XRCID("CassetteButton"), SessionPage::OnClickMediaMenu)
-	EVT_BUTTON(XRCID("BrowseDiskA"), SessionPage::OnBrowseDiskA)
-	EVT_BUTTON(XRCID("BrowseDiskB"), SessionPage::OnBrowseDiskB)
-	EVT_BUTTON(XRCID("BrowseCartA"), SessionPage::OnBrowseCartA)
-	EVT_BUTTON(XRCID("BrowseCartB"), SessionPage::OnBrowseCartB)
-	EVT_BUTTON(XRCID("BrowseCassette"), SessionPage::OnBrowseCassette)
-	EVT_BUTTON(XRCID("ClearDiskA"), SessionPage::OnEjectDiskA)
-	EVT_BUTTON(XRCID("ClearDiskB"), SessionPage::OnEjectDiskB)
-	EVT_BUTTON(XRCID("ClearCartA"), SessionPage::OnEjectCartA)
-	EVT_BUTTON(XRCID("ClearCartB"), SessionPage::OnEjectCartB)
-	EVT_BUTTON(XRCID("ClearCassette"), SessionPage::OnClearCassette)
-	EVT_BUTTON(XRCID("RewindButton"), SessionPage::OnRewind)
-	EVT_MENU(Disk_Insert_New, SessionPage::OnInsertEmptyDiskByMenu)
-	EVT_MENU(Disk_Browse_File, SessionPage::OnBrowseDiskByMenu)
-	EVT_MENU(Disk_Browse_Dir, SessionPage::OnBrowseDiskDirByMenu)
-	EVT_MENU(Disk_Browse_Ips, SessionPage::OnBrowseIps)
-	EVT_MENU(Disk_Eject, SessionPage::OnEjectByMenu)
-	EVT_MENU(Cart_Browse_File, SessionPage::OnBrowseCartByMenu)
-	EVT_MENU(Cart_Eject, SessionPage::OnEjectByMenu)
+	EVT_COMBOBOX    (XRCID("DiskAContents"),    SessionPage::OnClickDiskACombo)
+	EVT_COMBOBOX    (XRCID("DiskBContents"),    SessionPage::OnClickDiskBCombo)
+	EVT_COMBOBOX    (XRCID("CartAContents"),    SessionPage::OnClickCartACombo)
+	EVT_COMBOBOX    (XRCID("CartBContents"),    SessionPage::OnClickCartBCombo)
+	EVT_COMBOBOX    (XRCID("CassetteContents"), SessionPage::OnClickCassetteCombo)
+	EVT_TOGGLEBUTTON(XRCID("PlayButton"),       SessionPage::OnModePlay)
+	EVT_TOGGLEBUTTON(XRCID("RecordButton"),     SessionPage::OnModeRecord)
+	EVT_BUTTON      (XRCID("DiskA_Button"),     SessionPage::OnClickMediaMenu)
+	EVT_BUTTON      (XRCID("DiskB_Button"),     SessionPage::OnClickMediaMenu)
+	EVT_BUTTON      (XRCID("CartA_Button"),     SessionPage::OnClickMediaMenu)
+	EVT_BUTTON      (XRCID("CartB_Button"),     SessionPage::OnClickMediaMenu)
+	EVT_BUTTON      (XRCID("CassetteButton"),   SessionPage::OnClickMediaMenu)
+	EVT_BUTTON      (XRCID("BrowseDiskA"),      SessionPage::OnBrowseDiskA)
+	EVT_BUTTON      (XRCID("BrowseDiskB"),      SessionPage::OnBrowseDiskB)
+	EVT_BUTTON      (XRCID("BrowseCartA"),      SessionPage::OnBrowseCartA)
+	EVT_BUTTON      (XRCID("BrowseCartB"),      SessionPage::OnBrowseCartB)
+	EVT_BUTTON      (XRCID("BrowseCassette"),   SessionPage::OnBrowseCassette)
+	EVT_BUTTON      (XRCID("ClearDiskA"),       SessionPage::OnEjectDiskA)
+	EVT_BUTTON      (XRCID("ClearDiskB"),       SessionPage::OnEjectDiskB)
+	EVT_BUTTON      (XRCID("ClearCartA"),       SessionPage::OnEjectCartA)
+	EVT_BUTTON      (XRCID("ClearCartB"),       SessionPage::OnEjectCartB)
+	EVT_BUTTON      (XRCID("ClearCassette"),    SessionPage::OnClearCassette)
+	EVT_BUTTON      (XRCID("RewindButton"),     SessionPage::OnRewind)
+	EVT_COMBOBOX    (XRCID("MachineList"),      SessionPage::OnMachineOrExtensionListChanged)
+	EVT_LISTBOX     (XRCID("ExtensionList"),    SessionPage::OnMachineOrExtensionListChanged)
+	EVT_MENU(Disk_Insert_New,    SessionPage::OnInsertEmptyDiskByMenu)
+	EVT_MENU(Disk_Browse_File,   SessionPage::OnBrowseDiskByMenu)
+	EVT_MENU(Disk_Browse_Dir,    SessionPage::OnBrowseDiskDirByMenu)
+	EVT_MENU(Disk_Browse_Ips,    SessionPage::OnBrowseIps)
+	EVT_MENU(Disk_Eject,         SessionPage::OnEjectByMenu)
+	EVT_MENU(Cart_Browse_File,   SessionPage::OnBrowseCartByMenu)
+	EVT_MENU(Cart_Eject,         SessionPage::OnEjectByMenu)
 	EVT_MENU(Cart_Select_Mapper, SessionPage::OnSelectMapper)
-	EVT_MENU(Cart_Browse_Ips, SessionPage::OnBrowseIps)
-	EVT_MENU(Cas_Browse_File, SessionPage::OnBrowseCassette)
-	EVT_MENU(Cas_Eject, SessionPage::OnClearCassette)
-	EVT_MENU(Cas_Rewind, SessionPage::OnRewind)
-	EVT_MENU(Cas_MotorControl, SessionPage::OnMotorControl)
+	EVT_MENU(Cart_Browse_Ips,    SessionPage::OnBrowseIps)
+	EVT_MENU(Cas_Browse_File,    SessionPage::OnBrowseCassette)
+	EVT_MENU(Cas_Eject,          SessionPage::OnClearCassette)
+	EVT_MENU(Cas_Rewind,         SessionPage::OnRewind)
+	EVT_MENU(Cas_MotorControl,   SessionPage::OnMotorControl)
 	EVT_MENU(Cas_AutoCreateFile, SessionPage::OnAutoCassettefile)
 END_EVENT_TABLE()
 
@@ -655,7 +657,7 @@ wxArrayString SessionPage::getHardware() const
 {
 	wxArrayString result;
 	int pos = m_machineList->GetSelection();
-	result.Add(pos == 0
+	result.Add(pos <= 0
 		? wxT(" <default> ")
 		: utils::ConvertPathNoSlash(m_machineArray[pos - 1]));
 
@@ -665,6 +667,11 @@ wxArrayString SessionPage::getHardware() const
 		result.Add(m_extensionArray[i]);
 	}
 	return result;
+}
+
+void SessionPage::OnMachineOrExtensionListChanged(wxCommandEvent& event)
+{
+	SaveHistory();
 }
 
 void SessionPage::AddHistory(MediaInfo& m)

src/SessionPage.h
@@ -125,6 +125,7 @@ private:
 	void OnBrowseCartByMenu(wxCommandEvent& event);
 	void OnEjectByMenu(wxCommandEvent& event);
 	void OnSelectMapper(wxCommandEvent& event);
+	void OnMachineOrExtensionListChanged(wxCommandEvent& event);
 
 	void SetupRomType(wxString romtype, wxString fullname);
 	MediaInfo* GetLastMenuTarget() const;