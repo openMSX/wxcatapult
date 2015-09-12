ae15aa5263c08883f15ab7aaf1b99ac13fb98d98
m9710797 <vermaelen.wouter@gmail.com>
2015-09-12 17:11:14 +0200

    goto release mode / update version number / fill-in release date / copy release-notes to release-history

--- build/version.py
+++ build/version.py
@@ -12,10 +12,10 @@ import re
 packageName = 'catapult'
 
 # Version number.
-packageVersion = '0.11.0'
+packageVersion = '0.12.0'
 
 # Is this a release version ("True") or development version ("False").
-releaseFlag = False
+releaseFlag = True
 
 # TODO: Before extraction of SVN or git-SVN revision number can be done, we
 #       should figure out a way to avoid rewriting Version.ii on every build.
