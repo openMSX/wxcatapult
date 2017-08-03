92f2825a5b2e7a4bddf6ac6f97ee1ce858393776
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2017-08-03 12:11:20 +0200

    Prepare for real release 0.14.0.

--- build/version.py
+++ build/version.py
@@ -12,7 +12,7 @@ import re
 packageName = 'catapult'
 
 # Version number.
-packageVersion = '0.14.0-rc1'
+packageVersion = '0.14.0'
 
 # Is this a release version ("True") or development version ("False").
 releaseFlag = True
