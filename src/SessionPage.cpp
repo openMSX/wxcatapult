openMSX/wxcatapult

commit 1c08101d1e99367f30790ff3d7284f31cf2f6ea7

Make Catapult compatible again with wx-3.0...
This was unfortunately a not backwards compatible change in wx-3.0, so
preprocessor tricks were needed (as per advice of the wxWidgets devs).

src/SessionPage.cpp
@@ -713,8 +713,13 @@ void SessionPage::RestoreHistory()
 				type,
 				split(ips, wxT(";;")))); // 2nd-level
 		}
-
-		if ((insertedMedia & m->mediaBits) && !m->control.IsEmpty()) {
+		if ((insertedMedia & m->mediaBits) &&
+#if wxVERSION_NUMBER < 2903
+				!m->control.IsEmpty()
+#else
+				!m->control.IsTextEmpty()
+#endif
+				) {
 			m->control.SetSelection(0);
 			auto* h = static_cast<HistoryData*>(m->control.GetClientObject(0));
 			if (m->mediaType == CARTRIDGE) SetMapperType(*m, h->type);

commit 6fdb4b5d817ce4d19596ec4d019a776cffa24314

Fixed last history item not shown in media combobox
Got broken in 1c08101d, where the wrong getter was used for wx-3.0. People
started noticing when the Windows build was migrated to wx-3.0. But the bug is
that old...

src/SessionPage.cpp
@@ -694,13 +694,7 @@ void SessionPage::RestoreHistory()
 				type,
 				split(ips, wxT(";;")))); // 2nd-level
 		}
-		if ((insertedMedia & m->mediaBits) &&
-#if wxVERSION_NUMBER < 2903
-				!m->control.IsEmpty()
-#else
-				!m->control.IsTextEmpty()
-#endif
-				) {
+		if ((insertedMedia & m->mediaBits) && !m->control.IsListEmpty()) {
 			m->control.SetSelection(0);
 			auto* h = static_cast<HistoryData*>(m->control.GetClientObject(0));
 			if (m->mediaType == CARTRIDGE) SetMapperType(*m, h->type);