5a7eb21073633e83f06c88d25a06b062c6471e37
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2020-08-23 10:57:04 +0200

    Update flag to C++17
    This is also used for openMSX anyway.

--- build/platform-linux.mk
+++ build/platform-linux.mk
@@ -9,4 +9,4 @@ EXEEXT:=
 # The next line is needed to determine the name of the XRC library from wx-config.
 WX2XRC_DYNAMIC:=s/-lwx_\\([^-]*\\)-\\([^ ]*\\)/& -lwx_\\1_xrc-\\2/
 
-CXXFLAGS+= -std=c++0x
+CXXFLAGS+= -std=c++17
