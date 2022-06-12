18c75ba34da47b077b530266a71203afa97c2f54
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2022-06-12 15:35:34 +0200

    Final changes for releasing 18.0.

--- build/version.mk
+++ build/version.mk
@@ -4,7 +4,7 @@
 PACKAGE_NAME:=openmsx-catapult
 
 # Version number.
-PACKAGE_VERSION:=18.0-rc2
+PACKAGE_VERSION:=18.0
 
 # Is this a release version ("true") or development version ("false").
 RELEASE_FLAG:=true
